6424085bcb1220ba91a618e9130597593c838c93
qpotak <pechenkinmaxim8@gmail.com>
2024-11-17 22:12:20 +0300

    change file

--- PyQtUSE/mainwindow1.py
+++ PyQtUSE/mainwindow1.py
@@ -6,11 +6,14 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main_window1.ui', self)
-        self.pushButton.clicked.connect(self.run)
 
-    def run(self):
-        pass
+        self.stackedWidget.setCurrentWidget(self.mainmenu)
+
+        self.pushButton.clicked.connect(self.runrus)
+
+    def runrus(self):
         self.label.setText("OK")
+        self.stackedWidget.setCurrentWidget(self.variantsrus)
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
