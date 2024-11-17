843ec8ee471f4aa952a69ae7bbc07981c9f6916f
qpotak <pechenkinmaxim8@gmail.com>
2024-11-17 23:27:03 +0300

    add page

--- PyQtUSE/mainwindow1.py
+++ PyQtUSE/mainwindow1.py
@@ -2,21 +2,33 @@ import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
+
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main_window1.ui', self)
+        uic.loadUi('main_window2.ui', self)
 
         self.stackedWidget.setCurrentWidget(self.mainmenu)
 
-        self.pushButton.clicked.connect(self.runrus)
+        self.pushButton.clicked.connect(self.runrusvar)
+        self.startrus_btn.clicked.connect(self.startrus)
+        self.v1r_btn_to_vr.clicked.connect(self.runrusvar)
+        self.v2r_btn_to_vr.clicked.connect(self.runrusvar)
 
-    def runrus(self):
+    def runrusvar(self):
         self.label.setText("OK")
         self.stackedWidget.setCurrentWidget(self.variantsrus)
 
+    def startrus(self):
+        if self.radioButton_r1.isChecked():
+            self.stackedWidget.setCurrentWidget(self.variant1rus)
+        if self.radioButton_r2.isChecked():
+            self.stackedWidget.setCurrentWidget(self.variant2rus)
+
+
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
