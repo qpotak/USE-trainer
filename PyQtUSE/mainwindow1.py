4459315f07f100ba4c6a11e868236016aa2bd02a
qpotak <pechenkinmaxim8@gmail.com>
2024-11-21 01:47:11 +0300

    change file

--- PyQtUSE/mainwindow1.py
+++ PyQtUSE/mainwindow1.py
@@ -2,17 +2,17 @@ import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
-from ui_main_window2 import Ui_MainWindow
+from ui_main_window import Ui_MainWindow
 from PyQt6.QtGui import QAction
-
+import sqlite3
 
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        #self.setupUi(self)
-        uic.loadUi('main_window3.ui',self)
+        self.setupUi(self)
+        #uic.loadUi('main_window3.ui',self)
 
         self.stackedWidget.setCurrentWidget(self.mainmenu)
 
@@ -25,7 +25,12 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.pushButton.clicked.connect(self.runrusvar)
         self.startrus_btn.clicked.connect(self.startrus)
 
+        self.con = sqlite3.connect('use_tasks.db')
 
+    def loadvr(self):
+        cur = self.con.cursor()
+        query = """SELECT task_text, task_text_mid, task_text_mid2, task_text_big FROM tasks WHERE variant = 1"""
+        res = cur.execute(query).fetchall()
     def runmainmenu(self):
         self.stackedWidget.setCurrentWidget(self.mainmenu)
 
@@ -33,8 +38,13 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.stackedWidget.setCurrentWidget(self.variantsrus)
 
     def startrus(self):
+        cur = self.con.cursor()
+        query = """SELECT task_text, task_text_mid, task_text_mid2, task_text_big FROM tasks WHERE variant = 1"""
+        res = cur.execute(query).fetchall()
+        self.stackedWidget.setCurrentWidget(self.variant1rus)
         if self.radioButton_r1.isChecked():
-            self.stackedWidget.setCurrentWidget(self.variant1rus)
+            self.vrRusTsk1_txt.setText(res[0][0])
+            self.vrRusTsk1_txt_big.setText(res[0][3])
         if self.radioButton_r2.isChecked():
             self.stackedWidget.setCurrentWidget(self.variant2rus)
 
